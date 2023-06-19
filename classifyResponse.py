9571d2630a128be1242ec011523ecdab47f1cdf3
soumboussaha <soumaya.boussaha@eurecom.fr>
2023-06-19 12:55:10 +0000

    added privacy filtering

--- classifyResponse.py
+++ classifyResponse.py
@@ -7,37 +7,14 @@ df = pd.read_csv('domains_with_owners.csv')
 # Create the third column and initialize it with empty strings
 df['Value'] = ''
 
-# Specify the valid org values
-valid_org_values = [
-    "Privacy service provided by Withheld for Privacy ehf",
-    "Contact Privacy Inc. Customer 0130027497",
-    "Contact Privacy Inc. Customer 0141730466",
-    "Contact Privacy Inc. Customer 7151571251",
-    "Jewella Privacy LLC Privacy ID# 10439376",
-    "Whois Privacy (enumDNS dba)",
-    "Digital Privacy Corporation",
-    "Contact Privacy Inc. Customer 0165685487",
-    "Contact Privacy Inc. Customer 0165632015",
-    "privacymanager.io",
-    "Domains By Proxy, LLC",
-    "NameBrightPrivacy.com",
-    "Whois Privacy Protection Service by onamae.com",
-    "Redacted for Privacy Purposes",
-    "Domain Privacy Service FBO Registrant.",
-    "Contact Privacy Inc. Customer 0147711685",
-    "Jewella Privacy LLC Privacy ID# 12406208",
-    "Contact Privacy Inc. Customer 0164203610",
-    "GLOBAL DOMAIN PRIVACY SERVICES INC",
-    "Whois Privacy - Com Laude",
-    "Super Privacy Service LTD c/o Dynadot",
-    "Contact Privacy Inc. Customer 0165685485",
-    "Contact Privacy Inc. Customer 0159414542",
-    "See PrivacyGuardian.org",
-    "DOMAIN PRIVACY SERVICE FBO REGISTRANT",
-    "MyPrivacy.net Ltd.",
-    "Data Protect",
-    "REDACTED FOR PRIVACY"
-]
+
+
+
+# Read the CSV file containing valid org values
+valid_org_values_df = pd.read_csv('valid_org_values.csv')
+
+# Extract the org values from the DataFrame
+valid_org_values = valid_org_values_df['Value'].tolist()
 
 # Iterate over each row in the DataFrame
 for index, row in df.iterrows():
